68bbc1ad395bb9069aa22cd86bdbd9498e185e12
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-02-26 23:03:24 +0500

    birthday exmple solved

--- js-lessons/script.js
+++ js-lessons/script.js
@@ -425,3 +425,18 @@ function breakingRecords(scores) {
 }
 console.log(breakingRecords(score));
 */
+let s = [2, 2, 1, 3, 2];
+function birthday(s, d, m) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    let sum = 0;
+    for (let j = i; j < i + m; j++) {
+      sum = sum + s[j];
+    }
+    if (sum == d) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(birthday(s, 4, 2));
