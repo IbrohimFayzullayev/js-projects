2043b025683c0cdc97909b9d6265307127239901
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-03-01 19:56:06 +0500

    new files added

---
+++ js-exaples/script.js
@@ -0,0 +1,12 @@
+let words = "is2 Thi1s T4est 3a";
+
+let wordsArr = words.split(" ");
+console.log(wordsArr);
+let l = wordsArr.length;
+let cn = 1;
+for (let i = 0; i < wordsArr.length; i++) {
+  for (let j = 0; j < wordsArr[i].length; j++) {
+    if (wordsArr[j] < cn) {
+    }
+  }
+}
