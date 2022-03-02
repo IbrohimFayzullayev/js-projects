23ac4054dd36671fecc8a8d8edb3894e218f8096
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-03-02 12:21:27 +0500

    new js code

--- js-exaples/script.js
+++ js-exaples/script.js
@@ -1,12 +1,38 @@
-let words = "is2 Thi1s T4est 3a";
-
-let wordsArr = words.split(" ");
-console.log(wordsArr);
-let l = wordsArr.length;
-let cn = 1;
-for (let i = 0; i < wordsArr.length; i++) {
-  for (let j = 0; j < wordsArr[i].length; j++) {
-    if (wordsArr[j] < cn) {
+// function squareDigits(num) {
+//   let nums = String(num).split("");
+//   let sum = "";
+//   for (let i = 0; i < nums.length; i++) {
+//     sum += String(Number(nums[i]) ** 2);
+//   }
+//   return sum;
+// }
+// console.log(squareDigits(9119));
+// let num = 9119;
+// let nums = String(num).split("");
+// console.log(nums);
+function XO(str) {
+  //code here
+  let s = str.toLowerCase();
+  let st = s.split("");
+  let x = 0;
+  let o = 0;
+  for (let i = 0; i < st.length; i++) {
+    if (st[i] == "x") {
+      x++;
+    } else if (st[i] == "o") {
+      o++;
     }
   }
+
+  if (x == 0 && o == 0) {
+    return true;
+  } else if (x == o) {
+    return true;
+  } else {
+    return false;
+  }
 }
+console.log(XO("xox"));
+let son = "GssdG";
+let ss = son.toLowerCase();
+console.log(ss);
