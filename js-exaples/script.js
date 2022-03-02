d3a776d29a469a139534118734d1a1d60832d3f1
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-03-02 22:09:36 +0500

    new commit

--- js-exaples/script.js
+++ js-exaples/script.js
@@ -10,29 +10,46 @@
 // let num = 9119;
 // let nums = String(num).split("");
 // console.log(nums);
-function XO(str) {
-  //code here
-  let s = str.toLowerCase();
-  let st = s.split("");
-  let x = 0;
-  let o = 0;
-  for (let i = 0; i < st.length; i++) {
-    if (st[i] == "x") {
-      x++;
-    } else if (st[i] == "o") {
-      o++;
-    }
-  }
+// console.log(XO("xoxo"));
+// function XO(str) {
+//   //code here
+//   let s = str.toLowerCase();
+//   let st = s.split("");
+//   let x = 0;
+//   let o = 0;
+//   for (let i = 0; i < st.length; i++) {
+//     if (st[i] == "x") {
+//       x++;
+//     } else if (st[i] == "o") {
+//       o++;
+//     }
+//   }
 
-  if (x == 0 && o == 0) {
-    return true;
-  } else if (x == o) {
-    return true;
-  } else {
-    return false;
-  }
-}
-console.log(XO("xox"));
-let son = "GssdG";
-let ss = son.toLowerCase();
-console.log(ss);
+//   if (x == 0 && o == 0) {
+//     return true;
+//   } else if (x == o) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// let son = "GssdG";
+// let ss = son.toLowerCase();
+// console.log(ss);
+// console.log(XO("salom"));
+// let trss = "xoxossld";
+// console.log(XO(trss));
+// let func = function (salom) {};
+// let myFunc = (sdd, ddd) => {
+
+// };
+// let myObject={
+//   boyi=1.7,
+//   eni=80,
+//   harf="salom",
+//   myfn=function(){
+//     let dd= myObject.boyi*myObject.eni;
+//     let ss=this.boyi+this.eni;
+//   }
+// }
