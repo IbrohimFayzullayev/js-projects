7b109f3b7c0c6d9bfdc01602947314823c32dde9
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-03-08 14:42:34 +0500

    new com

--- codewars/script.js
+++ codewars/script.js
@@ -45,31 +45,72 @@
 // }
 // console.log(solution(str));
 
-// ** The Hashtag Generator 5kyu
-let txt = "codeWars sddfhg  fdggf";
-function generateHashtag(str) {
-  let arr = [];
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] == " ") {
-      continue;
-    } else if (str[i - 1] == " ") {
-      arr.push(str[i].toUpperCase());
-    } else {
-      arr.push(str[i]);
+// ** The Hashtag Generator 5kyu - solved
+// let txt = "codeWars sddfhg  fdggf";
+// function generateHashtag(str) {
+//   let arr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] == " ") {
+//       continue;
+//     } else if (str[i - 1] == " ") {
+//       arr.push(str[i].toUpperCase());
+//     } else {
+//       arr.push(str[i]);
+//     }
+//   }
+//   let gd = arr.join("");
+//   if (gd.length > 140) {
+//     return false;
+//   } else if (gd.length == 1) {
+//     return false;
+//   } else {
+//     let gd1 = ["#"];
+//     gd1.push(gd[0].toUpperCase());
+//     for (let i = 1; i < gd.length; i++) {
+//       gd1.push(gd[i]);
+//     }
+//     return gd1.join("");
+//   }
+// }
+// console.log(generateHashtag(txt));
+
+// ** Create Phone Number 6kyu
+// let art = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// function createPhoneNumber(numbers) {
+//   let result = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (i == 0) {
+//       result.push("(");
+//       result.push(numbers[0]);
+//     } else if (i == 2) {
+//       result.push(numbers[i]);
+//       result.push(") ");
+//     } else if (i == 5) {
+//       result.push(numbers[i]);
+//       result.push("-");
+//     } else {
+//       result.push(numbers[i]);
+//     }
+//   }
+//   return result.join("");
+// }
+// console.log(createPhoneNumber(art));
+
+// ** The Primes as a Result of the Longest Consecutive Sum I 5kyu
+let num = 100;
+function primeMaxLengthChain(n) {
+  var x;
+  let count = 0;
+  for (let i = 1; i < n; i++) {
+    for (let j = i; j > 0; j--) {
+      if (i % j == 0) {
+        count++;
+      }
+    }
+    if (count == 2) {
+      return i;
     }
   }
-  let gd = arr.join("");
-  if (gd.length > 140) {
-    return false;
-  } else if (gd.length == 1) {
-    return false;
-  } else {
-    let gd1 = ["#"];
-    gd1.push(gd[0].toUpperCase());
-    for (let i = 1; i < gd.length; i++) {
-      gd1.push(gd[i]);
-    }
-    return gd1.join("");
-  }
+  // return x; // or []
 }
-console.log(generateHashtag(txt));
+console.log(primeMaxLengthChain(num));
