e0130e969b727471a388d08e880f8f9e006df579
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-03-24 15:51:37 +0500

    lear prototype

--- oop_22_03_2022/script.js
+++ oop_22_03_2022/script.js
@@ -65,3 +65,40 @@
 // console.log(square.area);
 
 // // 23.03.2022
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person("Jonas", 2001);
+console.log(jonas);
+// new Person("john", 1998);
+
+// 1.New {} is created
+// 2.function is calles, this = {}
+// 3. {} linked to prototype
+// 4. function automacially return {}
+
+const matilda = new Person("Matilda", 2015);
+const jack = new Person("Jack", 2002);
+console.log(matilda, jack);
+
+console.log(matilda instanceof Person);
+
+// Prototypes
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
